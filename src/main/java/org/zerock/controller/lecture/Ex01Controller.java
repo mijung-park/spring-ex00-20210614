515f4c871a96186a5db5c5423b392c4b547de729
mijung-park <mezzang.park@gmail.com>
2021-06-17 15:47:10 +0900

    20210617

---
+++ src/main/java/org/zerock/controller/lecture/Ex01Controller.java
@@ -0,0 +1,46 @@
+package org.zerock.controller.lecture;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/ex01/*") // ()경로로 오면 controller가 작동함
+@Log4j
+public class Ex01Controller {
+
+	@RequestMapping("/sub01") // /ex01/sub01로 요청이 오면 이 method가 작동함
+	public void method01() {
+//		System.out.println("ex01, sub01 method");
+		log.info("ex01, sub01 method");
+	}
+	
+	@RequestMapping("/sub02")
+	public void method02() {
+		log.info("ex01, sub02 method");
+	}
+	
+	@RequestMapping(value = "/sub03", method = RequestMethod.GET) // get 방식
+	public void method03() {
+		log.info("ex01, sub03 get method");
+	}
+	
+	@RequestMapping(value = "/sub03", method = RequestMethod.POST) // post 방식
+	public void method04() {
+		log.info("ex01, sub04 post method");
+	}
+	
+	@GetMapping("/sub05") 
+	public void method05() {
+		log.info("ex01, sub05 get method");
+	}
+	
+	@PostMapping("/sub06")
+	public void method06() {
+		log.info("ex01, sub06 post method");
+	}
+}
---
+++ src/main/java/org/zerock/controller/lecture/Ex04Controller.java
@@ -0,0 +1,41 @@
+package org.zerock.controller.lecture;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/ex04/*")
+@Log4j
+public class Ex04Controller {
+
+	@RequestMapping("/sub01")
+	public void method01(@RequestParam String date) {
+		log.info("ex04 sub01 method");
+		
+		log.info(date);
+	}
+	
+	@RequestMapping("/sub02") 
+	public void method02(@RequestParam Date date) {
+		log.info("ex04 sub02 methd");
+		log.info(date);
+	} // Date 자동형변환 X 
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		log.info("init biner");
+		
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(format, false));
+		//true, false로 필수 여부를 넣어줌 (이경우에는 false이므로 필수X)
+	} // Init은 RequestMapping method 실행 전에 실행
+}
