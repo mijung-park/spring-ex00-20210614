2df62b4e87fbbf1acc743a4083b6fe283d13f856
mijung-park <mezzang.park@gmail.com>
2021-07-01 09:10:00 +0900

    20210630

--- src/main/webapp/WEB-INF/sql/SQL04.sql
+++ src/main/webapp/WEB-INF/sql/SQL04.sql
@@ -11,3 +11,7 @@ CREATE TABLE tbl_reply (
 SELECT * FROM tbl_board ORDER BY bno DESC;
 
 SELECT * FROM tbl_reply ORDER BY rno DESC;
+
+-- 댓글이 있는 게시물 조회
+SELECT DISTINCT (b.bno)
+FROM tbl_board b JOIN tbl_reply r ON b.bno=r.bno;
